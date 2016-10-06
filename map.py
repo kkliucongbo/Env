09f04d4b78a408a2913e8f71258519008b74f88c
kkliucongbo <492101725@qq.com>
2016-10-07 04:35:24 +0800

    基础hadoop MAP模板

--- map.py
+++ map.py
@@ -8,11 +8,9 @@ def read_input(file):
     for line in file:
         yield line.rstrip().split('\t')
 
-
 def main():
     data = read_input(sys.stdin)
     for key,kviter in groupby(data,itemgetter(0)):
-
-
+        
 if __name__ == '__main__':
     main()
